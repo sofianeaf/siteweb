80a4816b3c9f8ac563e4503e4ab7f6010b10cae7
Sofiane <sofianeaffes@gmail.com>
2020-04-20 23:31:29 +0200

    blog: added clickable title with a collapse

--- hidden/blog/js/script.js
+++ hidden/blog/js/script.js
@@ -1,11 +1,8 @@
 
 var myArray = [
-    {'date':'1989/10/11', 'topic':'zeb', 'author':'Sofiane Affes', 'platform':'youtube'},
-    {'date':'1989/01/10', 'topic':'nom', 'author':'Fawzi Nounchaku', 'platform':'youtube'},
-    {'date':'1990/14/10', 'topic':'nouna', 'author':'Sofiane Affes', 'platform':'youtube'},
-    {'date':'1993/29/11', 'topic':'zeb', 'author':'Samir Zantou7a', 'platform':'dailymotion'},
-    {'date':'1991/12/03', 'topic':'zeb', 'author':'Farid Frite', 'platform':'youtube'},
-    {'date':'1991/12/10', 'topic':'nouna', 'author':'7mid 103', 'platform':'website'},
+    {'title':'The 3 Stages Of Wealth EVERYONE Must Go Through', 'date':'2020/20/04', 'topic':'business', 'author':'Alex Becker', 'platform':'youtube' , 'link':'https://www.youtube.com/watch?v=Upu5VdT56jc', 'comment':'Type of business depending on where one is at.'},
+    {'title':'Why Your Mind Is Full Of Weeds', 'date':'2020/20/04', 'topic':'business', 'author':'Sam Ovens', 'platform':'youtube' , 'link':'https://www.youtube.com/watch?v=sslfgLMOyWY', 'comment':'Focus on the most important things.'},
+
 ]
 
 $('#search-input').on('keyup', function(){
@@ -22,11 +19,12 @@ function searchTable(value, data){
 
   for (var i = 0; i < data.length; i++){
     value = value.toLowerCase()
+    var title = data[i].title.toLowerCase()
     var topic = data[i].topic.toLowerCase()
     var author = data[i].author.toLowerCase()
     var platform = data[i].platform.toLowerCase()
 
-    if (topic.includes(value) || author.includes(value) || platform.includes(value)){
+    if (title.includes(value) || topic.includes(value) || author.includes(value) || platform.includes(value)){
       filteredData.push(data[i])
     }
   }
@@ -58,12 +56,18 @@ function buildTable(data){
     var table = document.getElementById('myTable')
     table.innerHTML = ''
     for (var i = 0; i < data.length; i++){
+        var coltitle = `title-${i}`
         var coldate = `date-${i}`
         var coltopic = `topic-${i}`
         var colauthor = `author-${i}`
         var colplatform = `platform-${i}`
 
         var row = `<tr>
+                        <td>
+                          <a data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                            ${data[i].title}
+                          </a>
+                        </td>
                         <td>${data[i].date}</td>
                         <td>${data[i].topic}</td>
                         <td>${data[i].author}</td>
